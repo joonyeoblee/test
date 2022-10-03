1567ac018d855c9b20d4362e1fe0b041e338b9b2
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2022-10-03 12:40:26 +0900

    Update NewBehaviourScript.cs
    11

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -27,3 +27,23 @@ public class NewBehaviourScript : MonoBehaviour
     
     }
 }
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Quaternion quaternion = Quaternion.identity;
+        transform.rotation = Quaternion.Euler(0,0,10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+*/
