44e8b1c70560d3120d15a98da4a5e24872c00aef
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2022-12-02 17:05:25 +0900

    마무리

---
+++ Assets/Scripts/homeworkScript/Target.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private float timer;
+    public float downTime;
+
+    public bool isUpped;
+    public bool gameStart= false;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        //�ö�������� Ư�� �� �ڿ� ������
+        if (gameStart&&isUpped)
+        {
+            timer += Time.deltaTime;
+            if (timer >= downTime)
+            {
+                timer = 0;
+                SetDown();
+            }
+        }
+    }
+    //�Ѹ�����
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Arrow")
+        {
+            SetIsHit();
+        }
+    }
+
+    //�¾��� �� �������°�
+    public void SetIsHit()
+    {
+        Debug.Log(transform + "Hit");
+        isUpped = false;
+        anim.SetBool("isHit", !isUpped);
+        timer = 0f;
+        TargetController.Instance.ScorePlus();
+        TargetController.Instance.isNextTarget = true;
+    }
+    public void SetUp()
+    {
+        isUpped = true;
+        anim.SetBool("isHit", !isUpped);
+    }
+
+    public void SetDown()
+    {
+        isUpped = false;
+        anim.SetBool("isHit", !isUpped);
+        TargetController.Instance.isNextTarget = true;
+    }
+}
---
+++ Assets/Scripts/homeworkScript/TargetController.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BNG;
+using TMPro;
+using System;
+
+public class TargetController : MonoBehaviour
+{
+    private float timer;
+    
+    public TextMeshPro hitText;
+    public TextMeshPro missText;
+    public TextMeshPro bulletText;
+    public static TargetController Instance { get; private set; }
+
+    public Target[] targets;
+
+    public float downTime;
+
+    public bool isNextTarget;
+
+    public int targetLeft;
+    public int score = 0;
+    public int useBullet = 0;
+
+    public ButtonController buttonController;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        RaycastWeapon.OnPistolFired += RaycastWeapon_OnPistolFired;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isNextTarget)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (timer > 2f)
+        {
+            TryUpTarget();
+            timer = 0;
+        }
+    }
+
+    public void StartGame(float downTime)
+    {
+        targetLeft = 10;
+
+        isNextTarget = true;
+        foreach (Target t in targets)
+        {
+            t.downTime = downTime;
+        }
+        PutDownAllTarget();
+
+        hitText.text = ""; 
+        missText.text = "";
+        bulletText.text = "";
+    }
+
+    private void TryUpTarget()
+    {
+        if(targetLeft > 0)
+        {
+            targetLeft--;
+            RamdomlyUpTarget();
+        }
+        else
+        {
+            StopGame();
+        }
+    }
+
+    //9개 타겟 중 하나 무작위로 올려줌
+    public void RamdomlyUpTarget()
+    {
+        isNextTarget = false;
+        int i = UnityEngine.Random.Range(0, 9);
+        targets[i].SetUp();
+        
+    }
+
+    public void StopGame()
+    {
+        buttonController.StopGame();
+        //게임끝나면 전부 올리기
+        PutUpAllTarget();
+        //결과 띄우기
+        hitText.text = "Hit score\n" + score.ToString(); 
+        missText.text = "Miss score\n" + (10 - score).ToString(); 
+        bulletText.text = "Using bullet\n" + useBullet; 
+    }
+
+    public void PutDownAllTarget()
+    {
+        foreach(Target t in targets)
+        {
+            t.SetDown();
+            t.gameStart = true;
+        }
+    }
+    public void PutUpAllTarget()
+    {
+        foreach(Target t in targets)
+        {
+            t.SetUp();
+            t.gameStart = false;
+        }
+    }
+     public void ScorePlus()
+    {
+       score++;
+    }
+    public void RaycastWeapon_OnPistolFired(object sender, EventArgs e)
+    {
+        if(buttonController.isGameStart)
+            useBullet++;
+    }
+
+}
